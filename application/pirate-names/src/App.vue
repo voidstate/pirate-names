0fbe9c8ce344f3fb23c32e2263399abd1eb21a0c
Fergus Hadley <ferg@voidstate.com>
2025-02-28 13:57:21 +0000

    Initial commit

---
+++ application/pirate-names/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Generator from './components/Generator.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>PNG</h1>
+    </div>
+  </header>
+
+  <main>
+    <Generator />
+  </main>
+</template>
+
+<style scoped></style>

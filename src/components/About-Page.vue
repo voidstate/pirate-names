c0d17310f11e4e4a836ccc2cd111dd899d73b2ba
Fergus Hadley <ferg@voidstate.com>
2026-08-03 15:16:30 +0000

    edited about page

--- src/components/About-Page.vue
+++ src/components/About-Page.vue
@@ -6,7 +6,7 @@
 			<h2>What's this Pirate Name Generator all about, then?</h2>
 
 			<p>As we all know, sometimes you just need a pirate, fast. Sure, you could ask one of those new-fangled LLMs but where's the fun in that? This little treasure trove of an app keeps things simple, by combining a hand-curated selection of piratey names and words together for your personal edification.</p>
-			<p>And now you have a pirate that you're happy with, why not see what shenanegans he gets up to in a roleplaying game? Maybe the wonderful <a href="https://www.limithron.com/pirateborg">Pirate Borg RPG</a>?</p>
+			<p>And now you have a pirate  that you're happy with (or even a whole crew of them), why not see what shenanegans he (or they) gets up to in a roleplaying game? Maybe the wonderful <a href="https://www.limithron.com/pirateborg">Pirate Borg RPG</a>?</p>
 			<p><a href="https://www.limithron.com/pirateborg"><img src="/src/assets/images/bitmaps/pirate-borg/pirate-borg-compatible.png" alt="Pirate Borg" class="app__about-pirate-borg"></a></p>
 
 	</div>

9222815f04d2c4460040719c1655d4a61b98dab0
Fergus Hadley <ferg@voidstate.com>
2026-08-03 09:28:11 +0000

    Github pages fix

--- src/router.js
+++ src/router.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+const getBasePath = () => {
+	const redirect = window.location.search.includes('redirect=')
+		? new URLSearchParams(window.location.search).get('redirect')
+		: ''
+
+	return redirect ? redirect : '/'
+}
+
 import MainPage from './components/Main-Page.vue'
 import AboutPage from './components/About-Page.vue'
 
@@ -12,5 +20,14 @@ const router = createRouter({
 	history: createWebHashHistory(),
 	routes,
 })
+
+router.beforeEach( ( to, from, next ) => {
+	const redirect = new URLSearchParams( window.location.search ).get( 'redirect' )
+	if ( redirect && to.path === '/' ) {
+		next( { path: redirect, replace: true } )
+		return
+	}
+	next()
+} )
 
 export default router

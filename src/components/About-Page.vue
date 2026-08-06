2c1219824d6876e7e0e7bf668a30c473e3cdde7b
Fergus Hadley <ferg@voidstate.com>
2026-08-06 12:46:26 +0000

    about page

--- src/components/About-Page.vue
+++ src/components/About-Page.vue
@@ -3,13 +3,11 @@
 <template>
 
 	<div class="about-page">
-			<h2>What's this Pirate Name Generator all about, then?</h2>
-
-			<p>As we all know, sometimes you just need a pirate, fast. Sure, you could ask one of those new-fangled LLMs but where's the fun in that? This little treasure trove of an app keeps things simple, by combining a hand-curated selection of piratey names and words together for your personal, piratey edification.</p>
+			<h2>What's this Pirate Crew Generator all about, then?</h2>
+			<p>As we all know, sometimes you just need a pirate, fast. Sure, you could ask one of those new-fangled LLMs but where's the fun in that (besides, water is for sailing on not coolong data centres!)? This little treasure trove of an app keeps things simple, by combining a hand-curated selection of piratey names and words together for your personal, piratey edification.</p>
 			<p>And now you have a pirate  that you're happy with (or even a whole crew of them), why not discover what shenanegans he (or they) gets up to in a roleplaying game? Maybe the wonderful <a href="https://www.limithron.com/pirateborg">Pirate Borg RPG</a>?</p>
 			<p><a href="https://www.limithron.com/pirateborg" class="about-page__pirate-borg-link">Set sail for the Pirate Borg website!</a></p>
 			<p><a href="https://www.limithron.com/pirateborg"><img src="/src/assets/images/bitmaps/pirate-borg/pirate-borg-compatible.png" alt="Pirate Borg" class="about-page__pirate-borg-img"></a></p>
-
 	</div>
 
 </template>
